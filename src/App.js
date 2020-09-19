70b33cf27331a5b6f1687be92a8c2d8ab23facea
ico4e <70281513+ico4e@users.noreply.github.com>
2020-09-18 21:59:12 -0400

    v0.0.1

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          Learn React
+          hi{' '}
+          <span role='img' aria-labelledby='hand'>
+            ✌
+          </span>{' '}
+          salva
         </a>
       </header>
     </div>
